1a11c84f72387901799fe8aea9d80890d55bdb59
Sefa Sia Deniz <sefasaiddeniz1@gmail.com>
2017-03-13 18:59:12 +0300

    Sayfadaki isim id lerinin alınması yapıldı

--- Model/botUsersId.js
+++ Model/botUsersId.js
@@ -6,11 +6,12 @@
 
 
 var mongoose = require('mongoose');
-
+mongoose.connect('mongodb://localhost:27017/imdbb');
 var Schema = mongoose.Schema;
 
 var botUserId = new Schema({
-    id: String
+    id: String,
+    name : String
 });
 
 
--- usersBot.js
+++ usersBot.js
@@ -4,5 +4,35 @@
 
 var request             = require('request'); // İstek yapmak için kullanılacak paket.
 var cheerio             = require('cheerio'); // Getirilen veriyi parçalamak için kullanılacak paket.
-var botIds          = require('./Model/botUsersId');
-var botUsersSetting      = require('./Model/botUsersSetting');
+var botIds              = require('./Model/botUsersId');
+var botUsersSetting     = require('./Model/botUsersSetting');
+
+var imdbUrl = "http://www.imdb.com/search/name?has=awards,bio,birth-date,death-date,height,quotes&sort=alpha&start=";
+var countInPage = 50;
+var page = 0;
+var start = countInPage * page;
+
+request(imdbUrl + start, function (error, response, html) {
+
+    if ( ! error && response.statusCode == 200) {
+
+        var $ = cheerio.load(html);
+
+        $('td.name').each(function(i, element){
+
+            var data = $(this);
+
+            // In examining the DOM we notice that the title rests within the first child element of the header tag.
+            // Utilizing jQuery we can easily navigate and get the text by writing the following code:
+
+            var id = data.children().first().attr('href');
+            var name = data.children().first().text();
+
+            // Once we have our title, we'll store it to the our json object.
+            var imdbId = id.split("/");
+            console.log(imdbId[2]);
+        });
+
+    }else{
+    }
+});
