7e6707e22c4289e094fb4bfc8518ed4f11634c14
Sefa Sia Deniz <sefasaiddeniz1@gmail.com>
2017-04-08 23:40:45 +0300

    Tested module if it’s work or not

--- test-rediction.js
+++ test-rediction.js
@@ -4,10 +4,14 @@
 var mongoose = require("mongoose");
 // mongo connection
 mongoose.connect("mongodb://localhost:27017/imdbcsv");
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-    // we're connected!
-    console.log("Connected correctly to server");
-});
 
+var limdu = require('limdu');
+
+var test = new limdu.classifiers.NeuralNetwork();
+test.trainBatch([
+    {input: { r: 0.03, g: 0.7, b: 0.5 }, output: 0},  // black
+    {input: { r: 0.16, g: 0.09, b: 0.2 }, output: 1}, // white
+    {input: { r: 0.5, g: 0.5, b: 1.0 }, output: 1}   // white
+]);
+
+console.log(test.classify({ r: 1, g: 0.4, b: 0 }));  // 0.99 - almost white
