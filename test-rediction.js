829def2cc6263203c779feecf98cc64d4ac88974
Sefa Sia Deniz <sefasaiddeniz1@gmail.com>
2017-04-08 23:57:36 +0300

    Added schema for db

--- test-rediction.js
+++ test-rediction.js
@@ -1,11 +1,56 @@
 /**
  * Created by sefasaid on 08/04/2017.
  */
-var mongoose = require("mongoose");
-// mongo connection
-mongoose.connect("mongodb://localhost:27017/imdbcsv");
-
+var mongoose = require('mongoose');
+var mongoosePaginate = require('mongoose-paginate');
+mongoose.connect('mongodb://localhost/imdbcsv');
 var limdu = require('limdu');
+
+var Schema = mongoose.Schema;
+
+var filmSchema = new Schema({
+
+    color:{type:String},
+    director_name : {type:String},
+    num_critic_for_reviews: {type:Number},
+    duration:{type:Number},
+    director_facebook_likes : {type:Number},
+    actor_3_facebook_likes : {type:Number},
+    actor_2_name : {type:Number},
+    actor_1_facebook_likes: {type:Number},
+    gross:{type:Number},
+    genres:{type:String},
+    actor_1_name:{type:String},
+    movie_title:{type:String},
+    num_voted_users:{type:Number},
+    cast_total_facebook_likes:{type:Number},
+    actor_3_name:{type:String},
+    facenumber_in_poster:{type:Number},
+    plot_keywords:{type:String},
+    movie_imdb_link:{type:String},
+    num_user_for_reviews:{type:Number},
+    language:{type:String},
+    country:{type:String},
+    content_rating:{type:String},
+    budget:{type:Number},
+    title_year:{type:Number},
+    actor_2_facebook_likes:{type:Number},
+    imdb_score:{type:Number},
+    aspect_ratio:{type:Number},
+    movie_facebook_likes:{type:Number}
+
+}, { timestamps: { createdAt: 'created_at', updatedAt : 'updated_at' } });
+filmSchema.plugin(mongoosePaginate);
+
+var Film = mongoose.model('denemee', filmSchema);
+Film.find(function (err,data) {
+    if(err)
+        console.log(err);
+    console.log(data);
+})
+
+
+/*
 
 var test = new limdu.classifiers.NeuralNetwork();
 test.trainBatch([
@@ -15,3 +60,5 @@ test.trainBatch([
 ]);
 
 console.log(test.classify({ r: 1, g: 0.4, b: 0 }));  // 0.99 - almost white
+
+    */
