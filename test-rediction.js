011f374ceca23afd3fa0c1eca78df3e009e04ddd
Sefa Sia Deniz <sefasaiddeniz1@gmail.com>
2017-04-08 23:38:10 +0300

    Added mongoldb connection

--- test-rediction.js
+++ test-rediction.js
@@ -1,3 +1,13 @@
 /**
  * Created by sefasaid on 08/04/2017.
  */
+var mongoose = require("mongoose");
+// mongo connection
+mongoose.connect("mongodb://localhost:27017/imdbcsv");
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    // we're connected!
+    console.log("Connected correctly to server");
+});
+
